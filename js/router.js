b521bd1dc0b03bffd8ddf1daf0304122893c7b06
Pecca556 <lpywpecca@gmail.com>
2025-05-13 20:08:52 +0200

    Revert "nigabalz"
    This reverts commit 68431c179a9d39d2c9e793703d46688623aa579e.

--- js/router.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const path = window.location.pathname;
-    
-    if (path === '/free-video-shoot' && !document.URL.includes('index-freevideo.html')) {
-        window.location.href = '/index-freevideo.html';
-    } else if (path === '/' && !document.URL.includes('index.html')) {
-        window.location.href = '/index.html';
-    }
-});
